d8712da292c0ed5b453e5690f6cfc04bcdd3a472
Marshan Eric <marshansr@gmail.com>
2022-12-10 23:43:17 +0200

    add /useres route

--- src/app/Http/Controller/User/UserController.php
+++ src/app/Http/Controller/User/UserController.php
@@ -4,17 +4,29 @@ declare(strict_types=1);
 
 namespace App\Http\Controller\User;
 
-use App\Domain\Entity\User;
+use App\Domain\Repository\UserRepository;
 use App\Http\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
+    public function __construct(
+        private UserRepository $userRepository,
+        RequestStack $requestStack
+    ) {
+        parent::__construct($requestStack);
+    }
+
     #[Route('/users', methods: ['HEAD', 'GET'], name: 'pages.users.index')]
     public function showIndex(): Response
     {
-        return $this->render('pages/users/index.twig');
+        $users = $this->userRepository->findAll();
+
+        return $this->render('pages/users/index.twig', [
+            'users' => $users,
+        ]);
     }
 
     #[Route('/users/{id<\d+>}', methods: ['HEAD', 'GET'], name: 'pages.users.single')]
