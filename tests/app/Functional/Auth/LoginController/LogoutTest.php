19fa842d2a8483a6ae543e4204666c71b81b1925
Marshan Eric <marshansr@gmail.com>
2023-01-04 19:06:14 +0200

    test /logout route

---
+++ tests/app/Functional/Auth/LoginController/LogoutTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\App\Functional\Auth\LoginController;
+
+use App\Domain\Entity\User;
+use App\Domain\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class LogoutTest extends WebTestCase
+{
+    private UserRepository $userRepository;
+    private KernelBrowser $client;
+    private User $user;
+
+    protected function setUp(): void
+    {
+        $this->client = self::createClient();
+
+        /** @var UserRepository */
+        $this->userRepository = self::getContainer()->get(UserRepository::class);
+
+        $this->user = new User();
+
+        $this->user
+            ->setEmail('test1@test.com')
+            ->setPassword('123')
+            ->addVerifiedRole()
+            ->addBasedRole()
+        ;
+
+        $this->userRepository->save($this->user);
+        $this->userRepository->flush();
+
+        $this->client->loginUser($this->user);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        unset($this->client);
+        unset($this->user);
+    }
+
+    public function test_logout(): void
+    {
+        $this->client->request('GET', '/logout');
+
+        $this->assertResponseRedirects('http://localhost/login');
+    }
+}
